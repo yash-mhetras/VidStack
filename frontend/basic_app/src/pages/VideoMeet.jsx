168e2d6c5ad9e04f318e8f18fd6108aa9f5ab19b
Yash Mhetras <yashrm2005@gmail.com>
2025-06-23 00:02:04 +0530

    new file

--- frontend/basic_app/src/pages/VideoMeet.jsx
+++ frontend/basic_app/src/pages/VideoMeet.jsx
@@ -12,9 +12,10 @@ import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
 import { Route, useNavigate } from 'react-router-dom';
+import server from '../environment';
 
 
-const server_url = "http://localhost:8000";
+const server_url = server;
 var connections = {};
 const peerConfigConnections = {
     "iceServers": [
